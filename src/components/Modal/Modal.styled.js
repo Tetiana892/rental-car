fce6c168a5257610658184ba4bda88e275c1712c
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-10-19 14:02:10 +0300

    add modal scroll

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,25 +1,63 @@
 import styled from '@emotion/styled';
 
 export const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100vw;
+  height: 100vh;
   position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.7);
+
+  z-index: 2000;
+
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 1000;
 `;
 
 export const ModalContainer = styled.div`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 24px;
-  padding-top: 20px;
-  overflow: auto;
-  position: relative;
-  width: 541px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 100%;
+  max-width: 335px;
+
+  min-height: 225px;
+  max-height: 80%;
+
+  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #3e85f3;
+    border-radius: 12px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #e7e5e5;
+    height: 112px;
+    border-radius: 12px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #7c7c7c;
+  }
+
+  @media screen and (min-width: 768px) {
+    max-width: 541px;
+  }
 `;
 
 export const ModalClose = styled.button`
